bfcf0f4b0d9099040b8c2788605d750b05be11d1
totto2727 <h_tsuchida@yumemi.co.jp>
2025-04-05 23:53:34 +0900

    feat: .npmrcの追加とpackage.jsonの依存関係の整理
    - .npmrcを追加し、@jsrのレジストリを設定
    - package.jsonを更新し、プロジェクト名を@totto2727-org/mcpに変更
    - 依存関係を整理し、@totto/functionを追加
    - pnpm-lock.yamlを更新し、新しい依存関係を反映
    - worker-configuration.d.tsを型定義の整理
    - index.tsを更新し、タイムスタンプ機能を強化

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,64 @@
 import OAuthProvider from '@cloudflare/workers-oauth-provider'
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js'
+import type { CallToolResult } from '@modelcontextprotocol/sdk/types.js'
+import { DateTime, Effect, Match, pipe } from '@totto/function/effect'
 import { McpAgent } from 'agents/mcp'
 import app from './app'
 
 export class MyMCP extends McpAgent {
   server = new McpServer({
-    name: 'Timestamp',
+    name: '@totto2727-org/mcp',
     version: '1.0.0',
   })
+  timezone: DateTime.TimeZone = DateTime.zoneMakeLocal()
+
+  #setTimezone(timezone: string) {
+    pipe(
+      DateTime.zoneFromString(timezone),
+      Effect.tap((timezone) => {
+        this.timezone = timezone
+      }),
+      Effect.runSync,
+    )
+  }
+
+  fetch(...args: [Request]): Response | Promise<Response> {
+    pipe(
+      Effect.fromNullable(args[0].cf?.timezone),
+      Effect.tap(Match.type().pipe(Match.when(Match.string, (timezone) => this.#setTimezone(timezone)))),
+      Effect.runSync,
+    )
+    // biome-ignore lint/style/noNonNullAssertion: <explanation>
+    return super.fetch!(...args)
+  }
 
   async init() {
-    this.server.tool('timestamp', {}, async () => ({
-      content: [{ type: 'text', text: String(Date.now()) }],
-    }))
+    const timezone = this.timezone
+
+    this.server.tool('timestamp', {}, async () => {
+      const effect = Effect.gen(function* () {
+        const now = DateTime.toParts(DateTime.setZone(yield* DateTime.now, timezone))
+
+        const year = now.year.toString().padStart(4, '0')
+        const month = now.month.toString().padStart(2, '0')
+        const day = now.day.toString().padStart(2, '0')
+        const hours = now.hours.toString().padStart(2, '0')
+        const minutes = now.minutes.toString().padStart(2, '0')
+        const seconds = now.seconds.toString().padStart(2, '0')
+
+        const timestamp = `${year}${month}${day}${hours}${minutes}${seconds}`
+
+        return {
+          content: [{ type: 'text', text: timestamp }],
+        } satisfies CallToolResult
+      })
+      return await Effect.runPromise(effect)
+    })
   }
 }
 
-// Export the OAuth handler as the default
 export default new OAuthProvider({
   apiRoute: '/sse',
-  // TODO: fix these types
   // @ts-ignore
   apiHandler: MyMCP.mount('/sse'),
   // @ts-ignore
